d02f822180751ef402b40538573015173f251fcf
Anna <annabenson@MacBook-Pro-4.local>
2015-09-23 18:55:19 -0400

    Starting Project

---
+++ CancerInstituteDemo/CancerInstituteDemo/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  CancerInstituteDemo
+//
+//  Created by Anna Benson on 9/23/15.
+//  Copyright © 2015 KAG. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
