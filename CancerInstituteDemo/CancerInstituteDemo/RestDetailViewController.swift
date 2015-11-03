763be390980110493dffdfb0b3754fb002eca302
Anna <annabenson@MacBook-Pro-20.local>
2015-11-03 05:51:08 -0500

    Added Rest, Relax, Renue Navigation

---
+++ CancerInstituteDemo/CancerInstituteDemo/RestDetailViewController.swift
@@ -0,0 +1,33 @@
+//
+//  RestDetailsViewController.swift
+//  
+//
+//  Created by Anna Benson on 11/2/15.
+//
+//
+
+import UIKit
+
+class RestDetailViewController: UIViewController {
+    
+    var myTitle: String?
+    var myDescription: String?
+    var myImage: String?
+    
+    @IBOutlet weak var curTitle: UILabel!
+    @IBOutlet weak var curDescription: UILabel!
+    @IBOutlet weak var curImage: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        curTitle.text = myTitle
+        curDescription.text = myDescription
+        curImage.image = UIImage(named: myImage!)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+
+}
