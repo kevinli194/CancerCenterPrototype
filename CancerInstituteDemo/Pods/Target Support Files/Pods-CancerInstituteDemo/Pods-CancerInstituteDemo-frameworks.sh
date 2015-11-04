c22caf11c2297272e358cde6c66cc5c338203b67
gtnet8@gmail.com <gtnet8@gmail.com>
2015-11-04 02:48:00 -0500

    removed RKView

--- CancerInstituteDemo/Pods/Target Support Files/Pods-CancerInstituteDemo/Pods-CancerInstituteDemo-frameworks.sh
+++ CancerInstituteDemo/Pods/Target Support Files/Pods-CancerInstituteDemo/Pods-CancerInstituteDemo-frameworks.sh
@@ -84,8 +84,10 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
-  install_framework "Pods-CancerInstituteDemo/RKCardView.framework"
+  install_framework "Pods-CancerInstituteDemo/Alamofire.framework"
+  install_framework "Pods-CancerInstituteDemo/CVCalendar.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
-  install_framework "Pods-CancerInstituteDemo/RKCardView.framework"
+  install_framework "Pods-CancerInstituteDemo/Alamofire.framework"
+  install_framework "Pods-CancerInstituteDemo/CVCalendar.framework"
 fi
