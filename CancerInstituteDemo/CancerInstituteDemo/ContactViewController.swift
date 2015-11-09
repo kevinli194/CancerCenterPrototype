c746c9bac8024150e14623e545d6fd05a847146f
Anna <annabenson@MacBook-Pro-20.local>
2015-11-09 18:01:14 -0500

    a few changes to the main storyboard

--- CancerInstituteDemo/CancerInstituteDemo/ContactViewController.swift
+++ CancerInstituteDemo/CancerInstituteDemo/ContactViewController.swift
@@ -10,36 +10,49 @@ import UIKit
 
 class ContactViewController: UIViewController {
     
-    @IBAction func callGeorgia(sender: UIButton) {
-        let url:NSURL = NSURL(string:"tel://4042478954")!
+    @IBOutlet weak var box1: UIImageView!
+    @IBOutlet weak var box2: UIImageView!
+    @IBOutlet weak var box3: UIImageView!
+    
+    @IBAction func callTriage(sender: AnyObject) {
+        let url:NSURL = NSURL(string: "tel://9196686608")!
         UIApplication.sharedApplication().openURL(url)
     }
-
-    @IBAction func callJon(sender: UIButton) {
-        let url:NSURL = NSURL(string:"tel://5183690814")!
+    
+    @IBAction func callResourceCenter(sender: AnyObject) {
+        let url:NSURL = NSURL(string:"tel://9196846955")!
+        UIApplication.sharedApplication().openURL(url)
+    }
+    
+    @IBAction func callConcierge(sender: AnyObject) {
+        let url:NSURL = NSURL(string:"tel://9196814947")!
         UIApplication.sharedApplication().openURL(url)
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = UIColor(patternImage: UIImage(named: "trees.png")!)
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        box1.layer.cornerRadius = 10
+        box1.clipsToBounds = true
+        box2.layer.cornerRadius = 10
+        box2.clipsToBounds = true
+        box3.layer.cornerRadius = 10
+        box3.clipsToBounds = true
+        
     }
     
-
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    
     /*
     // MARK: - Navigation
-
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    // Get the new view controller using segue.destinationViewController.
+    // Pass the selected object to the new view controller.
     }
     */
-
+    
 }
