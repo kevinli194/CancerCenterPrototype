0255bcc757f16f7aff0eb4403cbf53365664a23c
Anna <annabenson@MacBook-Pro-20.local>
2015-11-17 17:19:25 -0500

    with defult person image

--- CancerInstituteDemo/CancerInstituteDemo/AppointmentController.swift
+++ CancerInstituteDemo/CancerInstituteDemo/AppointmentController.swift
@@ -42,21 +42,22 @@ class AppointmentController: UIViewController {
         }
         curClinic.text = myClinic
         
-        curImage.image = UIImage(named: "HFriedman.jpg")
-        
         //let myPhoto = photos[people.indexOf(curProvider.text!)!]
-        // curImage.image = UIImage(named: photos[people2.indexOf(curProvider.text!)!])
+        //curImage.image = UIImage(named: photos[people2.indexOf(curProvider.text!)!])
         //curDescription.text = descriptions[people2.indexOf(curProvider.text!)!]
         
         if curProvider.text!.containsString("A. Friedman"){
-        
             curDescription.text = "What we specialize in at The Preston Robert Tisch Brain Tumor Center is compassionate care – we take care of our patients. It’s complete care. The patients don’t have to go out and find a radiologist. We have the radiation oncologist, the chemotherapy, the surgery, the radiology, we have all kinds of nursing people as well as help getting physical therapy and getting in-the-home assistance. Patients also can expect cutting-edge care because we run such a terrific science program here. I really like working with the patients. No question about it. It’s not that I’m doing some trivial procedure six times a day. When someone comes in with a brain tumor, there’s usually some bonding, and instead of running away from that, I really enjoy it."
+            curImage.image = UIImage(named: "AFriedman.jpg")
         }
-        if curProvider.text!.containsString("A. Friedman"){
+        
+        else if curProvider.text!.containsString("H. Friedman"){
             curDescription.text = "I am an academic adult and pediatric neuro-oncologist who helps lead The Preston Robert Tisch Brain Tumor Center at Duke. I am the author of more than 500 peer-reviewed articles, reviews, and book chapters, and I have presented extensively at both international and national meetings. I am a committed educator who is a member of the executive and super-executive committees of the Duke University School of Medicine, which are responsible for the recruitment, screening and selection of medical students. I have reviewed grants for the National Institutes of Health for over 15 years, in a spectrum of different study sections. I am the senior editor of CNS Oncology, associate editor for the Neuro-Oncology Section of Cancer, and a reviewer for over 25 academic journals. I serve on the advisory board of a number of organizations, including Voices Against Brain Cancer, the Tug McGraw Foundation, Pinnacle Care, the Pediatric Brain Tumor Foundation, the National Children’s Cancer Society, and the American Brain Tumor Association."
+            curImage.image = UIImage(named: "HFriedman.jpg")
         }
         else {
             curDescription.text = "I am an academic adult and pediatric neuro-oncologist who helps lead The Preston Robert Tisch Brain Tumor Center at Duke. I am the author of more than 500 peer-reviewed articles, reviews, and book chapters, and I have presented extensively at both international and national meetings. I am a committed educator who is a member of the executive and super-executive committees of the Duke University School of Medicine, which are responsible for the recruitment, screening and selection of medical students. I have reviewed grants for the National Institutes of Health for over 15 years, in a spectrum of different study sections. I am the senior editor of CNS Oncology, associate editor for the Neuro-Oncology Section of Cancer, and a reviewer for over 25 academic journals. I serve on the advisory board of a number of organizations, including Voices Against Brain Cancer, the Tug McGraw Foundation, Pinnacle Care, the Pediatric Brain Tumor Foundation, the National Children’s Cancer Society, and the American Brain Tumor Association."
+            curImage.image = UIImage(named: "defultPhoto.jpg")
         }
     
     }
