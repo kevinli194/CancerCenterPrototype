ecbd666088deb5650d77bea6b055a6bda8a52308
gtnet8@gmail.com <gtnet8@gmail.com>
2015-11-17 23:25:16 -0500

    fiddling around with maps and angle

--- CancerInstituteDemo/CancerInstituteDemo/MapController.swift
+++ CancerInstituteDemo/CancerInstituteDemo/MapController.swift
@@ -14,9 +14,37 @@ class MapController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        //self.view.backgroundColor = UIColor.blackColor();
-        var overlay = GMSGroundOverlay()
-        let icon = UIImage(named: "newark_nj_1922.jpg")
+        
+        let camera = GMSCameraPosition.cameraWithLatitude(36.006725,
+            longitude: -78.937257, zoom: 16, bearing: -40, viewingAngle: 30)
+        let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
+        mapView.myLocationEnabled = true
+        mapView.settings.myLocationButton = true
+        mapView.settings.compassButton = true
+        mapView.setMinZoom(10, maxZoom: 24)
+        mapView.padding = UIEdgeInsetsMake(self.topLayoutGuide.length + 5,0,
+            self.bottomLayoutGuide.length + 5, 0);
+        self.view = mapView
+        
+//        let marker = GMSMarker()
+//        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+//        marker.title = "Sydney"
+//        marker.snippet = "Australia"
+//        marker.map = mapView
+//        
+        
+        let southWest = CLLocationCoordinate2DMake(36.004591, -78.936642)
+        let northEast = CLLocationCoordinate2DMake(36.00552, -78.936502)
+        let overlayBounds = GMSCoordinateBounds(coordinate: southWest, coordinate: northEast)
+        
+        let icon = UIImage(named: "Image")
+        
+        let overlay = GMSGroundOverlay(bounds: overlayBounds, icon: icon)
+        overlay.bearing = 0
+
+        overlay.map = mapView
+        
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,7 +52,7 @@ class MapController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
 
     
 }
