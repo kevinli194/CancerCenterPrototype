d1a83ecbce0b57b16080bfc4de4cacaa160bdb98
gtnet8@gmail.com <gtnet8@gmail.com>
2015-12-01 16:45:55 -0500

    added backlog and changed map

--- CancerInstituteDemo/CancerInstituteDemo/MapController.swift
+++ CancerInstituteDemo/CancerInstituteDemo/MapController.swift
@@ -19,34 +19,38 @@ class MapController: UIViewController {
         self.locationManager.requestWhenInUseAuthorization()
         
         let camera = GMSCameraPosition.cameraWithLatitude(36.006725,
-            longitude: -78.937257, zoom: 16, bearing: -20, viewingAngle: 30)
+            longitude: -78.937257, zoom: 16, bearing: -60, viewingAngle: 30)
         let mapView = GMSMapView.mapWithFrame(CGRectZero, camera: camera)
         mapView.myLocationEnabled = true
-        mapView.settings.myLocationButton = true
+        //mapView.settings.myLocationButton = true
         mapView.settings.compassButton = true
         mapView.setMinZoom(10, maxZoom: 24)
-        mapView.padding = UIEdgeInsetsMake(self.topLayoutGuide.length + 5,0,
-            self.bottomLayoutGuide.length + 20, 0);
+        //mapView.padding = UIEdgeInsetsMake(self.topLayoutGuide.length + 5,0,
+            //self.bottomLayoutGuide.length + 20, 0);
        
         
-        mapView.addObserver(self, forKeyPath: "myLocation", options: .New, context: nil)
+        //mapView.addObserver(self, forKeyPath: "myLocation", options: .New, context: nil)
         
         self.view = mapView
+    
         let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(36.005255, -78.934807)
+
+        
+        marker.position = CLLocationCoordinate2DMake(36.005369, -78.934438)
         marker.title = "Duke Medicine Circle Parking Garage"
         marker.snippet = "302 Trent Drive"
+        marker.icon = UIImage(named: "parking")
         marker.map = mapView
         
         
-        let southWest = CLLocationCoordinate2DMake(36.004910, -78.937428)
-        let northEast = CLLocationCoordinate2DMake(36.004910, -78.936784)
-        let overlayBounds = GMSCoordinateBounds(coordinate: southWest, coordinate: northEast)
+        let northEast = CLLocationCoordinate2DMake(36.005388, -78.937946)
+        let southWest = CLLocationCoordinate2DMake(36.004415, -78.936993)
+        let overlayBounds = GMSCoordinateBounds(coordinate: northEast, coordinate: southWest)
         
         let icon = UIImage(named: "Image")
         
         let overlay = GMSGroundOverlay(bounds: overlayBounds, icon: icon)
-        overlay.bearing = 0
+        overlay.bearing = -70
         
 
         overlay.map = mapView
